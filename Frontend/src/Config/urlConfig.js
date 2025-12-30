ad7e54e797ed351b8200fbd2a9766cb4f1c93d0c
govinda-mahanti <gmahanti955@gmail.com>
2025-12-30 14:58:52 +0530

    add

--- Frontend/src/Config/urlConfig.js
+++ Frontend/src/Config/urlConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://pastebin-backend-thk0.onrender.com/api";
 
 export const createPaste = (data) =>
   axios.post(`${BASE_URL}/paste`, data);
