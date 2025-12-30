6e4c6bad88bb1177239fc2f2d32757d0271d3d38
govinda-mahanti <gmahanti955@gmail.com>
2025-12-30 14:10:58 +0530

    add

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectRedis } from "./db/db.js";
+import { connectRedis } from "../db/db.js";
 import pastebinRoutes from "./routes/pastebinRoutes.js";
 
 dotenv.config();
 
 const app = express();
-
-/* ✅ CORS MUST BE FIRST */
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "OPTIONS"],
@@ -30,4 +28,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to Pastebin server");
 });
 
-export default app;   // 👈 important for Vercel
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
